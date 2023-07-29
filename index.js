f6f15defe3cde38384ff26bb8ee1aa1a9cf32ecb
SOOYASH-JAJU <92200282+SOOYASH-JAJU@users.noreply.github.com>
2023-07-29 20:01:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrumButtons;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+  {
+
+    var buttonInnerHTML=this.innerHTML;
+    // buttonInnerHTML.style.color="white";
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+
+  document.addEventListener("keydown",function(event)
+  {
+    makeSound(event.key);
+    buttonAnimation(buttonInnerHTML);
+  });
+
+function makeSound(key)
+{
+  var audio;
+  switch (key)
+  {
+    case 'w':
+    audio=new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+
+    case 'a':
+    audio=new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+
+    case 's':
+    audio=new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+
+    case 'd':
+    audio=new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+
+    case 'j':
+    audio=new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+
+    case 'k':
+    audio=new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+
+    case 'l':
+    audio=new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+
+    default:
+    console.log(key);
+
+  }
+}
+
+function buttonAnimation(key)
+{
+  var activeButton=document.querySelector("."+key);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function()
+{
+    activeButton.classList.remove("pressed");
+},222);
+
+}
+
+}
